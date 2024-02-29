9d02febfc4de1bc05fb2b1fa02a1b0f45a64873c
smarttechfire <educationfire552@gmail.com>
2024-02-29 17:02:16 +0530

    Clerk Authentication

--- app/(dashboard)/(routes)/files/page.js
+++ app/(dashboard)/(routes)/files/page.js
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Files() {
   return (
     <div>
       Files
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
